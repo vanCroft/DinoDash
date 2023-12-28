8c13d18e180ccc4890b2e5379349a92cf976b9ba
Rob vanCroft <rob.van.croft@googlemail.com>
2023-12-28 19:03:07 +0100

    StartMenü mit Background und Logo

--- app/src/main/java/com/example/dinodash/MainActivity.kt
+++ app/src/main/java/com/example/dinodash/MainActivity.kt
@@ -1,107 +1,101 @@
-package com.example.dinodash
+package com.example.com.example.dinodash
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import android.content.Intent
-import androidx.compose.ui.platform.LocalContext
-import com.example.dinodash.ui.theme.DinoDashTheme
-import androidx.compose.material3.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.tooling.preview.Preview
 
+import com.example.dinodash.GameActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DinoDashTheme {
-                // A surface container using a custom background color
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color(0xFF99cc99) // Custom background color
-                ) {
-                    MainMenu()
-                }
-            }
+            StartMenuScreen()
         }
     }
 }
 
 @Composable
-fun MainMenu() {
-    val context = LocalContext.current
-
-    // Custom button colors
-    val buttonColors = ButtonDefaults.buttonColors(
-        containerColor = Color(0xFF314A22), // Button background color
-        contentColor = Color.White // Text color
-    )
-
-    Column(
+fun StartMenuScreen() {
+    // Hintergrundbild + Logo
+    val backgroundImage: Painter = painterResource(id = R.drawable.dino_dash_bg)
+    val logoImage: Painter = painterResource(id = R.drawable.dino_dash_logo)
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(Color(0xFFA9B489))
     ) {
-        Spacer(modifier = Modifier.height(12.dp))
-        Button(
-            onClick = {
-                // Intent, um GameActivity zu starten
-                val intent = Intent(context, GameActivity::class.java)
-                context.startActivity(intent)
-            },
-            colors = buttonColors // Apply custom button colors
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        // Logo oben mittig platzieren
+        Image(
+            painter = logoImage,
+            contentDescription = "Dino Dash Logo",
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 123.dp) // Abstand von oben
+        )
+
+
+        // Zentriert den Button im Bildschirm
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
         ) {
-            Text(text = "DinoDash starten", style = TextStyle(
-                //fontFamily = FontFamily(Font(R.font.comic_sans)), // Verwendung von Comic Sans
-                fontWeight = FontWeight.Bold, // Fette Schrift
-                fontSize = 16.sp // Optional: Schriftgröße anpassen
-            ))
+            StartButton()
         }
-        Spacer(modifier = Modifier.height(12.dp))
-        Button(onClick = { /* TODO: Highscore Logic */ }, colors = buttonColors) {
-            Text(text = "Highscore")
-        }
-        Spacer(modifier = Modifier.height(12.dp))
-        Button(onClick = {
-            val intent = Intent(context, FaqActivity::class.java)
+    }
+}
+
+@Composable
+fun StartButton() {
+    // Zugriff auf den aktuellen Context
+    val context = LocalContext.current
+
+    Button(
+        onClick = {
+            //Starten des Spiels
+            val intent = Intent(context, GameActivity::class.java)
             context.startActivity(intent)
-        }, colors = buttonColors) {
-            Text(text = "FAQ")
-        }
-        Spacer(modifier = Modifier.height(12.dp))
-        Button(onClick = {
-            context.startActivity(Intent(context, SettingsActivity::class.java))
-        }, colors = buttonColors) {
-            Text(text = "Einstellungen")
-        }
+        },
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF314A22)), // Buttonfarbe
+        modifier = Modifier
+            .padding(16.dp)
+            .height(80.dp) // Höhe des Button
+    ) {
+        Text(
+            text = "DinoDash STARTEN",
+            color = Color.White, // Textfarbe
+            fontSize = 26.sp // Schriftgröße
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DinoDashTheme {
-        MainMenu()
-    }
+    StartMenuScreen ()
 }
